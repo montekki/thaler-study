d4bfcbeb6643f7bca8ff637bd2bd59929c97ac98
Fedor Sakharov <fedor.sakharov@gmail.com>
2022-12-06 08:48:33 +0300

    circuit to separate module

--- gkr-protocol/src/lib.rs
+++ gkr-protocol/src/lib.rs
@@ -1,7 +1,4 @@
-use std::{
-    cmp, iter,
-    ops::{Add, Mul},
-};
+use std::{cmp, iter};
 
 use ark_ff::{FftField, Field, Zero};
 use ark_poly::{
@@ -15,202 +12,23 @@ use sum_check_protocol::{
     VerifierRoundResult as SumCheckVerifierRoundResult,
 };
 
+mod circuit;
+
+use circuit::{Circuit, CircuitEvaluation};
+
 #[derive(Debug)]
 pub enum Error {}
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-/// A type of a gate in the Circuit.
-#[derive(Copy, Clone, PartialEq, Eq)]
-pub enum GateType {
-    /// An addition gate.
-    Add,
-
-    /// A multiplication gate.
-    Mul,
-}
-
-/// A gate in the Circuit.
-#[derive(Clone, Copy)]
-pub struct Gate {
-    /// A type of the gate.
-    ttype: GateType,
-
-    /// Two inputs, indexes into the previous layer gates outputs.
-    inputs: [usize; 2],
-}
-
-impl Gate {
-    /// Create a new `Gate`.
-    pub fn new(ttype: GateType, inputs: [usize; 2]) -> Self {
-        Self { ttype, inputs }
-    }
-}
-
-/// A layer of gates in the circuit.
-#[derive(Clone)]
-pub struct CircuitLayer {
-    layer: Vec<Gate>,
-}
-
-impl CircuitLayer {
-    /// Create a new `CircuitLayer`.
-    pub fn new(layer: Vec<Gate>) -> Self {
-        Self { layer }
-    }
-
-    /// The length of the layer.
-    pub fn len(&self) -> usize {
-        self.layer.len()
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.layer.is_empty()
-    }
-}
-
-/// An evaluation of a `Circuit` on some input.
-/// Stores every circuit layer interediary evaluations and the
-/// circuit evaluation outputs.
-pub struct CircuitEvaluation<F> {
-    /// Evaluations on per-layer basis.
-    pub layers: Vec<Vec<F>>,
-}
-
-impl<F: Copy> CircuitEvaluation<F> {
-    /// Takes a gate label and outputs the corresponding gate's value at layer `layer`.
-    pub fn w(&self, layer: usize, label: usize) -> F {
-        self.layers[layer][label]
-    }
-}
-
-/// The circuit in layered form.
-#[derive(Clone)]
-pub struct Circuit {
-    /// First layer being the output layer, last layer being
-    /// the input layer.
-    layers: Vec<CircuitLayer>,
-
-    /// Number of inputs
-    num_inputs: usize,
-}
-
-impl Circuit {
-    /// Evaluate a `Circuit` on a given input.
-    pub fn evaluate<F>(&self, input: &[F]) -> CircuitEvaluation<F>
-    where
-        F: Add<Output = F> + Mul<Output = F> + Copy,
-    {
-        let mut layers = vec![];
-        let mut current_input = input;
-
-        layers.push(input.to_vec());
-
-        for layer in self.layers.iter().rev() {
-            let temp_layer: Vec<_> = layer
-                .layer
-                .iter()
-                .map(|e| match e.ttype {
-                    GateType::Add => current_input[e.inputs[0]] + current_input[e.inputs[1]],
-                    GateType::Mul => current_input[e.inputs[0]] * current_input[e.inputs[1]],
-                })
-                .collect();
-
-            layers.push(temp_layer);
-            current_input = &layers[layers.len() - 1];
-        }
-
-        layers.reverse();
-        CircuitEvaluation { layers }
-    }
-
-    /// The $\text{add}_i(a, b, c)$ polynomial value at layer $i$.
-    pub fn add_i(&self, i: usize, a: usize, b: usize, c: usize) -> bool {
-        let gate = &self.layers[i].layer[a];
-
-        gate.ttype == GateType::Add && gate.inputs[0] == b && gate.inputs[1] == c
-    }
-
-    /// The $\text{mul}_i(a, b, c)$ polynomial value at layer $i$.
-    pub fn mul_i(&self, i: usize, a: usize, b: usize, c: usize) -> bool {
-        let gate = &self.layers[i].layer[a];
-
-        gate.ttype == GateType::Mul && gate.inputs[0] == b && gate.inputs[1] == c
-    }
-
-    pub fn num_outputs(&self) -> usize {
-        self.layers[0].layer.len()
-    }
-
-    fn add_i_ext<F: Field>(&self, r_i: &[F], i: usize) -> DenseMultilinearExtension<F> {
-        let mut add_i = vec![];
-        let num_vars_current = f64::from(self.layers[i].len() as u32).log2() as usize;
-
-        let num_vars_next = f64::from(
-            self.layers
-                .get(i + 1)
-                .map(|c| c.len())
-                .unwrap_or(self.num_inputs) as u32,
-        )
-        .log2() as usize;
-
-        for c in 0..2usize.pow(num_vars_next as u32) {
-            for b in 0..2usize.pow(num_vars_next as u32) {
-                for a in 0..2usize.pow(num_vars_current as u32) {
-                    add_i.push(match self.add_i(i, a, b, c) {
-                        true => F::one(),
-                        false => F::zero(),
-                    });
-                }
-            }
-        }
-
-        let add_i = DenseMultilinearExtension::from_evaluations_vec(
-            num_vars_current + num_vars_next * 2,
-            add_i,
-        );
-
-        add_i.fix_variables(r_i)
-    }
-
-    fn mul_i_ext<F: Field>(&self, r_i: &[F], i: usize) -> DenseMultilinearExtension<F> {
-        let mut mul_i = vec![];
-        let num_vars_current = f64::from(self.layers[i].len() as u32).log2() as usize;
-
-        let num_vars_next = f64::from(
-            self.layers
-                .get(i + 1)
-                .map(|c| c.len())
-                .unwrap_or(self.num_inputs) as u32,
-        )
-        .log2() as usize;
-
-        for c in 0..2usize.pow(num_vars_next as u32) {
-            for b in 0..2usize.pow(num_vars_next as u32) {
-                for a in 0..2usize.pow(num_vars_current as u32) {
-                    mul_i.push(match self.mul_i(i, a, b, c) {
-                        true => F::one(),
-                        false => F::zero(),
-                    });
-                }
-            }
-        }
-
-        let mul_i = DenseMultilinearExtension::from_evaluations_vec(
-            num_vars_current + num_vars_next * 2,
-            mul_i,
-        );
-
-        mul_i.fix_variables(r_i)
-    }
-}
-
 /// A $2k_{i+1}$ variate polynomial used for each step of GKR protocol.
 ///
 /// $$
 /// f^{i}_{r_i}(b, c) \coloneqq
-/// \widetilde{add}_i(r_i, b, c)(\tilde{W}\_{i+1}(b) + \tilde{W}\_{i+1}(c)) +
-/// \widetilde{mul}_i(r_i, b, c)(\tilde{W}\_{i+1}(b) \cdot \tilde{W}\_{i+1}(c))
+/// \widetilde{add}_i(r_i, b, c)(\tilde{W}\_{i+1}(b) +
+/// \tilde{W}\_{i+1}(c)) +
+/// \widetilde{mul}_i(r_i, b, c)(\tilde{W}\_{i+1}(b) \cdot
+/// \tilde{W}\_{i+1}(c))
 /// $$
 #[derive(Clone)]
 pub struct W<F: Field> {
@@ -366,8 +184,8 @@ impl<F: FftField> Verifier<F> {
         let mul_i = circuit.mul_i_ext(&r_zero, 0);
 
         let r = vec![r_zero];
-
         let m = vec![m_zero];
+
         Self {
             r,
             m,
@@ -551,13 +369,15 @@ impl<F: FftField> Prover<F> {
     /// At round $i$ a Sum-Check prover for polynomial
     /// $f^{(i)}_{r_i}(b, c)$.
     pub fn start_round(&mut self, i: usize, r_i: &[F]) {
-        let num_vars_current = f64::from(self.evaluation.layers[i].len() as u32).log2() as usize;
+        let num_vars_current = self.circuit.num_vars_at(i).unwrap();
 
-        let num_vars_next = f64::from(self.evaluation.layers[i + 1].len() as u32).log2() as usize;
+        let num_vars_next = self.circuit.num_vars_at(i + 1).unwrap();
+
         let w_b = DenseMultilinearExtension::from_evaluations_slice(
             num_vars_next,
             &self.evaluation.layers[i + 1],
         );
+
         self.w = w_b.clone();
         let w_c = w_b.clone();
         let mut add_i = vec![];
@@ -601,16 +421,7 @@ impl<F: FftField> Prover<F> {
     }
 
     pub fn round_msg(&mut self, j: usize) -> ProverMessage<F> {
-        if j == 2 * f64::from(
-            self.circuit
-                .layers
-                .get(self.i + 1)
-                .map(|c| c.len())
-                .unwrap_or(self.circuit.num_inputs) as u32,
-        )
-        .log2() as usize
-            - 1
-        {
+        if j == 2 * self.circuit.num_vars_at(self.i + 1).unwrap() - 1 {
             // The last round; do the polynomial restriction.
             let (b, c) = self.r.split_at(self.r.len() / 2);
 
@@ -655,88 +466,29 @@ mod tests {
     use ark_ff::{Fp64, MontBackend, MontConfig, PrimeField};
     use ark_poly::univariate::DensePolynomial;
     use ark_std::test_rng;
+    use circuit::circuit_from_book;
     use pretty_assertions::assert_eq;
+
+    use crate::circuit::{CircuitLayer, Gate, GateType};
 
     use super::*;
 
-    fn circuit_from_book() -> Circuit {
-        Circuit {
-            layers: vec![
-                CircuitLayer {
-                    layer: vec![
-                        Gate {
-                            ttype: GateType::Mul,
-                            inputs: [0, 1],
-                        },
-                        Gate {
-                            ttype: GateType::Mul,
-                            inputs: [2, 3],
-                        },
-                    ],
-                },
-                CircuitLayer {
-                    layer: vec![
-                        Gate {
-                            ttype: GateType::Mul,
-                            inputs: [0, 0],
-                        },
-                        Gate {
-                            ttype: GateType::Mul,
-                            inputs: [1, 1],
-                        },
-                        Gate {
-                            ttype: GateType::Mul,
-                            inputs: [1, 2],
-                        },
-                        Gate {
-                            ttype: GateType::Mul,
-                            inputs: [3, 3],
-                        },
-                    ],
-                },
-            ],
-            num_inputs: 4,
-        }
-    }
-
     fn three_layer_circuit() -> Circuit {
-        Circuit {
-            layers: vec![
-                CircuitLayer {
-                    layer: vec![
-                        Gate {
-                            ttype: GateType::Add,
-                            inputs: [0, 1],
-                        },
-                        Gate {
-                            ttype: GateType::Add,
-                            inputs: [2, 3],
-                        },
-                    ],
-                },
-                CircuitLayer {
-                    layer: vec![
-                        Gate {
-                            ttype: GateType::Add,
-                            inputs: [0, 1],
-                        },
-                        Gate {
-                            ttype: GateType::Add,
-                            inputs: [2, 3],
-                        },
-                        Gate {
-                            ttype: GateType::Add,
-                            inputs: [4, 5],
-                        },
-                        Gate {
-                            ttype: GateType::Add,
-                            inputs: [6, 7],
-                        },
-                    ],
-                },
+        Circuit::new(
+            vec![
+                CircuitLayer::new(vec![
+                    Gate::new(GateType::Add, [0, 1]),
+                    Gate::new(GateType::Add, [2, 3]),
+                ]),
+                CircuitLayer::new(vec![
+                    Gate::new(GateType::Add, [0, 1]),
+                    Gate::new(GateType::Add, [2, 3]),
+                    Gate::new(GateType::Add, [4, 5]),
+                    Gate::new(GateType::Add, [6, 7]),
+                ]),
             ],
-            num_inputs: 8,
-        }
+            8,
+        )
     }
 
     #[test]
@@ -783,34 +535,6 @@ mod tests {
         );
     }
 
-    /// A test of the circuit from figure 4.12
-    #[test]
-    fn circuit_test_from_book() {
-        let circuit = circuit_from_book();
-
-        let layers = circuit.evaluate(&[3, 2, 3, 1]);
-        assert_eq!(
-            layers.layers,
-            vec![vec![36, 6], vec![9, 4, 6, 1], vec![3, 2, 3, 1]]
-        );
-
-        // Test that mul_1 evaluates to 0 on all inputs except
-        // ((0, 0), (0, 0), (0, 0))
-        // ((0, 1), (0, 1), (0, 1))
-        // ((1, 0), (0, 1), (1, 0))
-        // ((1, 1), (1, 1), (1, 1))
-        for a in 0..4 {
-            for b in 0..4 {
-                for c in 0..4 {
-                    let expected = ((a == 0 || a == 1) && a == b && a == c)
-                        || a == 2 && b == 1 && c == 2
-                        || a == b && b == c && a == 3;
-                    assert_eq!(circuit.mul_i(1, a, b, c), expected, "{a} {b} {c}");
-                }
-            }
-        }
-    }
-
     #[test]
     fn protocol_test_from_book() {
         let rng = &mut test_rng();
@@ -847,19 +571,12 @@ mod tests {
 
         let mut verifier = Verifier::new(num_outputs, &circuit_outputs, circuit.clone(), rng);
 
-        for i in 0..circuit.layers.len() {
+        for i in 0..circuit.layers().len() {
             let r_i = verifier.r(i);
             prover.start_round(i, &r_i);
 
             let c_1 = prover.c_1();
-            let num_vars = 2 * f64::from(
-                circuit
-                    .layers
-                    .get(i + 1)
-                    .map(|c| c.len())
-                    .unwrap_or(circuit.num_inputs) as u32,
-            )
-            .log2() as usize;
+            let num_vars = 2 * circuit.num_vars_at(i + 1).unwrap();
 
             verifier.start_round(c_1, i, num_vars);
 
@@ -928,19 +645,12 @@ mod tests {
             rng,
         );
 
-        for i in 0..circuit.layers.len() {
+        for i in 0..circuit.layers().len() {
             let r_i = verifier.r(i);
             prover.start_round(i, &r_i);
 
             let c_1 = prover.c_1();
-            let num_vars = 2 * f64::from(
-                circuit
-                    .layers
-                    .get(i + 1)
-                    .map(|c| c.len())
-                    .unwrap_or(circuit.num_inputs) as u32,
-            )
-            .log2() as usize;
+            let num_vars = 2 * circuit.num_vars_at(i + 1).unwrap();
 
             verifier.start_round(c_1, i, num_vars);
 
