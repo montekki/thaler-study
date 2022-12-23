ce2a7a31e813166e43c3914073559b3050a51b61
montekki <montekki@users.noreply.github.com>
2022-12-23 06:47:17 +0000

    deploy: daf66b14f167932daca45a8b62d606b6042faed4

--- source-files.js
+++ source-files.js
@@ -4,7 +4,6 @@ var sourcesIndex = JSON.parse('{\
 "matrix_multiplication":["",[],["lib.rs"]],\
 "multilinear_extensions":["",[],["lib.rs"]],\
 "sum_check_protocol":["",[],["lib.rs"]],\
-"thaler_study":["",[],["lib.rs"]],\
 "triangle_counting":["",[],["lib.rs"]]\
 }');
 createSourceSidebar();
