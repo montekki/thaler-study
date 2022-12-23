b0bc072f8f41d937c247dca4e64dcbe71800ef34
montekki <montekki@users.noreply.github.com>
2022-12-23 06:13:17 +0000

    deploy: cccc66cd62b3f474a51381456c034b3de80857b1

--- implementors/core/marker/trait.StructuralEq.js
+++ implementors/core/marker/trait.StructuralEq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"gkr_protocol":[["impl&lt;F:&nbsp;Field&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"enum\" href=\"gkr_protocol/enum.ProverMessage.html\" title=\"enum gkr_protocol::ProverMessage\">ProverMessage</a>&lt;F&gt;"]]
+"gkr_protocol":[["impl&lt;F:&nbsp;Field&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"enum\" href=\"gkr_protocol/enum.ProverMessage.html\" title=\"enum gkr_protocol::ProverMessage\">ProverMessage</a>&lt;F&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
