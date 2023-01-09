8ecdaaefbe2872bbf5d4ed8140c9c98dc5b19dc5
montekki <montekki@users.noreply.github.com>
2023-01-09 06:46:25 +0000

    deploy: b73f65d33d5b00d19b286901ad754859d200337c

--- crates.js
+++ crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["fiat_shamir","gkr_protocol","matrix_multiplication","multilinear_extensions","sum_check_protocol","triangle_counting"];
+window.ALL_CRATES = ["fiat_shamir","gkr_protocol","matrix_multiplication","multilinear_extensions","relaxed_pcs","sum_check_protocol","triangle_counting"];
--- source-files.js
+++ source-files.js
@@ -3,6 +3,7 @@ var sourcesIndex = JSON.parse('{\
 "gkr_protocol":["",[],["circuit.rs","lib.rs","round_polynomial.rs"]],\
 "matrix_multiplication":["",[],["lib.rs"]],\
 "multilinear_extensions":["",[],["lib.rs"]],\
+"relaxed_pcs":["",[],["lib.rs","permutations.rs"]],\
 "sum_check_protocol":["",[],["lib.rs"]],\
 "triangle_counting":["",[],["lib.rs"]]\
 }');
