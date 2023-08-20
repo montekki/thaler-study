1580549a783ed965ccc74180a91a0bc7c32b3fdf
montekki <montekki@users.noreply.github.com>
2023-08-20 12:17:52 +0000

    deploy: b715c44b6e923f499ee458372d4e9181af22ce4e

--- implementors/core/marker/trait.StructuralPartialEq.js
+++ implementors/core/marker/trait.StructuralPartialEq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"gkr_protocol":[["impl&lt;F: Field&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.69.0/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"gkr_protocol/enum.ProverMessage.html\" title=\"enum gkr_protocol::ProverMessage\">ProverMessage</a>&lt;F&gt;"]]
+"gkr_protocol":[["impl&lt;F: Field&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.1/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"gkr_protocol/enum.ProverMessage.html\" title=\"enum gkr_protocol::ProverMessage\">ProverMessage</a>&lt;F&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/sum_check_protocol/trait.RngF.js
+++ implementors/sum_check_protocol/trait.RngF.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"fiat_shamir":[["impl&lt;F: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.69.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a>&gt; RngF&lt;F&gt; for <a class=\"struct\" href=\"fiat_shamir/struct.RandNums.html\" title=\"struct fiat_shamir::RandNums\">RandNums</a>&lt;F&gt;"]],
+"fiat_shamir":[["impl&lt;F: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a>&gt; RngF&lt;F&gt; for <a class=\"struct\" href=\"fiat_shamir/struct.RandNums.html\" title=\"struct fiat_shamir::RandNums\">RandNums</a>&lt;F&gt;"]],
 "sum_check_protocol":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
