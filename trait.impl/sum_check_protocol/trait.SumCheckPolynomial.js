008d6c877f3d7f2c6adc3834d069195ca069f25b
montekki <montekki@users.noreply.github.com>
2024-09-25 10:27:50 +0000

    deploy: da8d34cd859091058a36602f9e288c5261ba8c13

--- trait.impl/sum_check_protocol/trait.SumCheckPolynomial.js
+++ trait.impl/sum_check_protocol/trait.SumCheckPolynomial.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
-"matrix_multiplication":[["impl&lt;F: FftField&gt; <a class=\"trait\" href=\"sum_check_protocol/trait.SumCheckPolynomial.html\" title=\"trait sum_check_protocol::SumCheckPolynomial\">SumCheckPolynomial</a>&lt;F&gt; for <a class=\"struct\" href=\"matrix_multiplication/struct.G.html\" title=\"struct matrix_multiplication::G\">G</a>&lt;F&gt;"]],
+"matrix_multiplication":[["impl&lt;F: FftField&gt; SumCheckPolynomial&lt;F&gt; for <a class=\"struct\" href=\"matrix_multiplication/struct.G.html\" title=\"struct matrix_multiplication::G\">G</a>&lt;F&gt;"]],
 "sum_check_protocol":[],
-"triangle_counting":[["impl&lt;F: FftField&gt; SumCheckPolynomial&lt;F&gt; for <a class=\"struct\" href=\"triangle_counting/struct.G.html\" title=\"struct triangle_counting::G\">G</a>&lt;F&gt;"]]
+"triangle_counting":[["impl&lt;F: FftField&gt; <a class=\"trait\" href=\"sum_check_protocol/trait.SumCheckPolynomial.html\" title=\"trait sum_check_protocol::SumCheckPolynomial\">SumCheckPolynomial</a>&lt;F&gt; for <a class=\"struct\" href=\"triangle_counting/struct.G.html\" title=\"struct triangle_counting::G\">G</a>&lt;F&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
