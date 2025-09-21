cc4c8b9a7e302dd8194df8d7ecae53e991310e84
montekki <montekki@users.noreply.github.com>
2025-09-21 16:23:19 +0000

    deploy: 20236e926ee5d609729a46120a7743756c9a6e9d

--- trait.impl/sum_check_protocol/trait.RngF.js
+++ trait.impl/sum_check_protocol/trait.RngF.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["fiat_shamir",[["impl&lt;F: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a>&gt; <a class=\"trait\" href=\"sum_check_protocol/trait.RngF.html\" title=\"trait sum_check_protocol::RngF\">RngF</a>&lt;F&gt; for <a class=\"struct\" href=\"fiat_shamir/struct.RandNums.html\" title=\"struct fiat_shamir::RandNums\">RandNums</a>&lt;F&gt;"]]],["sum_check_protocol",[]]]);
+    var implementors = Object.fromEntries([["fiat_shamir",[["impl&lt;F: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a>&gt; <a class=\"trait\" href=\"sum_check_protocol/trait.RngF.html\" title=\"trait sum_check_protocol::RngF\">RngF</a>&lt;F&gt; for <a class=\"struct\" href=\"fiat_shamir/struct.RandNums.html\" title=\"struct fiat_shamir::RandNums\">RandNums</a>&lt;F&gt;"]]],["sum_check_protocol",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
